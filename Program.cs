820f36eae2c4d2ce2953ace8c14f645337ad583f
Gustavosouzadev <gustavoduti2003@gmail.com>
2021-03-20 00:58:12 -0300

    Finalizando AreaTrianguloRet

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AreTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            double Base;
+            double Altura;
+            const double Area = 2;
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Calcule a área de um triângulo retângulo: ");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("---OBS: Digite o numero e Pressione \"ENTER\"----");
+            Console.WriteLine("-----------------------------------------------");
+
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Base..: ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Base = Convert.ToDouble(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Altura: ");
+            
+            Console.ForegroundColor = ConsoleColor.White;
+            Altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.White;
+            double resultado = (Base * Altura) / Area;
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Area..: ");
+ 
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"{resultado:N1}");
+
+            Console.ResetColor();
+        }
+    }
+}
